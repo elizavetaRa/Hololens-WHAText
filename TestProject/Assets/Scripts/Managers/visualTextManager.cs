5c1a7bdf5de725b1c0d5fad1044ca07c6c1095f3
whatshungup <max.vollstaedt@gmx.de>
2018-01-08 16:24:44 +0100

    focused textArea-Object is accessible

--- TestProject/Assets/Scripts/Managers/visualTextManager.cs
+++ TestProject/Assets/Scripts/Managers/visualTextManager.cs
@@ -3,14 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using HoloToolkit.Unity.InputModule;
+
 public class VisualTextManager : Singleton<VisualTextManager>
 {
-    public string dummy = "Hundefutter";
+    //public string dummy = "Hundefutter";
     public GameObject textArea;
-    public GameObject LineRenderer;
+   //public GameObject LineRenderer;
+    private GazeManager gazeManager;
     // Use this for initialization
     void Start()
     {
+        Debug.Log( this.gameObject.transform.GetChild(0).name);
+        gazeManager = this.gameObject.transform.GetChild(0).gameObject.GetComponent<GazeManager>();
         //visualizeText(dummy);
     }
 
@@ -18,41 +23,47 @@ public class VisualTextManager : Singleton<VisualTextManager>
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-
-    internal void visualizeText(OcrResult ocrResult)
-    { // visualText.text = dummyText;
-        var headPosition = Camera.main.transform.position;
-        var gazeDirection = Camera.main.transform.forward;
-        var CameraToWorld = Camera.main.cameraToWorldMatrix;
-        var Projection = Camera.main.projectionMatrix;
-        float ImageWidth = Camera.main.pixelWidth;
-        float ImageHeight = Camera.main.pixelHeight;
-
-        Vector2 ImagePosZeroToOne = new Vector2(ocrResult.BoundingBox.x / ImageWidth, 1.0f - (ocrResult.BoundingBox.y / ImageHeight));
-        Vector2 ImagePosProjected = ((ImagePosZeroToOne * 2.0f) - new Vector2(1, 1)); // -1 to 1 space
-        Vector3 CameraSpacePos = UnProjectVector(Projection, new Vector3(ImagePosProjected.x, ImagePosProjected.y, 1));
-        Vector3 WorldSpaceRayPoint1 = CameraToWorld.MultiplyVector(new Vector4(0, 0, 0, 1));// camera location in world space
-        Vector3 WorldSpaceRayPoint2 = CameraToWorld.MultiplyVector(CameraSpacePos); // ray point in world space
-
-
-        RaycastHit hitInfo;
-        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        GameObject focusedObject = gazeManager.HitObject;
+        if (focusedObject != null && focusedObject.tag == "textArea")
         {
-            Debug.Log("Raycast hit!");
-            GameObject newArea = Instantiate(textArea);
-            TextMesh visualText = newArea.transform.Find("3DTextPrefab").gameObject.GetComponent<TextMesh>();
-            visualText.text = ocrResult.Text;
-            newArea.transform.position = hitInfo.point; //new Vector3(headPosition.x, headPosition.y, headPosition.z + 3);
-
-            Quaternion toQuat = Camera.main.transform.localRotation;
-            toQuat.x = 0;
-            toQuat.z = 0;
-            newArea.transform.rotation = toQuat;
+            callTextArea(focusedObject);
+            //Debug.Log(gazeManager.HitObject.transform.Find("3DTextPrefab").gameObject.GetComponent<TextMesh>().text);
         }
+        
     }
+
+
+    //internal void visualizeText(OcrResult ocrResult)
+    //{ // visualText.text = dummyText;
+    //    var headPosition = Camera.main.transform.position;
+    //    var gazeDirection = Camera.main.transform.forward;
+    //    var CameraToWorld = Camera.main.cameraToWorldMatrix;
+    //    var Projection = Camera.main.projectionMatrix;
+    //    float ImageWidth = Camera.main.pixelWidth;
+    //    float ImageHeight = Camera.main.pixelHeight;
+
+    //    Vector2 ImagePosZeroToOne = new Vector2(ocrResult.BoundingBox.x / ImageWidth, 1.0f - (ocrResult.BoundingBox.y / ImageHeight));
+    //    Vector2 ImagePosProjected = ((ImagePosZeroToOne * 2.0f) - new Vector2(1, 1)); // -1 to 1 space
+    //    Vector3 CameraSpacePos = UnProjectVector(Projection, new Vector3(ImagePosProjected.x, ImagePosProjected.y, 1));
+    //    Vector3 WorldSpaceRayPoint1 = CameraToWorld.MultiplyVector(new Vector4(0, 0, 0, 1));// camera location in world space
+    //    Vector3 WorldSpaceRayPoint2 = CameraToWorld.MultiplyVector(CameraSpacePos); // ray point in world space
+
+
+    //    RaycastHit hitInfo;
+    //    if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+    //    {
+    //        Debug.Log("Raycast hit!");
+    //        GameObject newArea = Instantiate(textArea);
+    //        TextMesh visualText = newArea.transform.Find("3DTextPrefab").gameObject.GetComponent<TextMesh>();
+    //        visualText.text = ocrResult.Text;
+    //        newArea.transform.position = hitInfo.point; //new Vector3(headPosition.x, headPosition.y, headPosition.z + 3);
+
+    //        Quaternion toQuat = Camera.main.transform.localRotation;
+    //        toQuat.x = 0;
+    //        toQuat.z = 0;
+    //        newArea.transform.rotation = toQuat;
+    //    }
+    //}
 
 
     internal void visualizeText(CameraPositionResult cameraPositionResult)
@@ -91,8 +102,8 @@ public class VisualTextManager : Singleton<VisualTextManager>
 
 
             GameObject newArea = Instantiate(textArea);
-            //TextMesh visualText = newArea.transform.Find("3DTextPrefab").gameObject.GetComponent<TextMesh>();
-            //visualText.text = ocrResult.Text;
+            TextMesh visualText = newArea.transform.Find("3DTextPrefab").gameObject.GetComponent<TextMesh>();
+            visualText.text = ocrResult.Text;
             newArea.transform.position = hitInfo.point; //new Vector3(headPosition.x, headPosition.y, headPosition.z + 3);
 
             Quaternion toQuat = Camera.main.transform.localRotation;
@@ -115,6 +126,12 @@ public class VisualTextManager : Singleton<VisualTextManager>
         from.y = (to.y - (from.z * axsY.z)) / axsY.y;
         from.x = (to.x - (from.z * axsX.z)) / axsX.x;
         return from;
+    }
+
+    internal void callTextArea(GameObject focusedObject)
+    {
+
+        focusedObject.transform.Find("3DTextPrefab").SendMessageUpwards("OnFocused", SendMessageOptions.DontRequireReceiver);
     }
 }
 
---
+++ TestProject/Assets/Scripts/Utils/textAreaScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textAreaScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnFocused()
+    {
+        TextMesh textBox = this.gameObject.transform.Find("3DTextPrefab").gameObject.GetComponent<TextMesh>();
+        Debug.Log(textBox.text);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
